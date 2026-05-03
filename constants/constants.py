410705c001aa2d1834447e68d239cc80bf260158
Miguel Rodriguez <marodriguez148+personal@gmail.com>
2026-05-03 12:56:24 -0400

    Adding contansts file

---
+++ constants/constants.py
@@ -0,0 +1,16 @@
+MEMBER_FACING_PORTAL_URL = "https://myezra-staging.ezra.com/"
+USER_FACING_PORTAL_URL = "https://staging-hub.ezra.com/sign-in/"
+DEFAULT_QA_PASSWORD = "Hemogoblin!1"
+
+TEST_CREDENTIALS = {
+    "default_qa_user": {
+        "username": "qa@function_ezra.com",
+        "password": DEFAULT_QA_PASSWORD,
+        "userrole": "member"
+    },
+    "default_qa_admin_user": {
+        "username": "michael.krakovsky+test_interview@functionhealth.com",
+        "password": "12121212Aa",
+        "userrole": "provider"
+    }
+}
