c79c35cffecc82c381cce1f0e15ea1d13a561623
Miguel Rodriguez <marodriguez148+personal@gmail.com>
2026-05-04 20:28:11 -0400

    Adding user to constant file

--- constants/constants.py
+++ constants/constants.py
@@ -8,6 +8,11 @@ TEST_CREDENTIALS = {
         "password": DEFAULT_QA_PASSWORD,
         "userrole": "member"
     },
+    "second_qa_user": {
+        "username": "qa2@function_ezra.com",
+        "password": DEFAULT_QA_PASSWORD,
+        "userrole": "member"
+    },
     "default_qa_admin_user": {
         "username": "michael.krakovsky+test_interview@functionhealth.com",
         "password": "12121212Aa",
